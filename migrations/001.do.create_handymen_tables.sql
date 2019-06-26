3561503c10b4d80825a4f59b8fd6ebfe1e81e6c7
Kyler <Krenneker16@gmail.com>
2019-06-26 00:10:30 -0400

    added migrations

--- migrations/001.do.create_handymen_tables.sql
+++ migrations/001.do.create_handymen_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE users
 (
     id SERIAL PRIMARY KEY,
     user_name TEXT NOT NULL UNIQUE,
-    title TEXT NOT NULL,
+    full_name TEXT NOT NULL,
     content TEXT,
     date_created TIMESTAMP DEFAULT now() NOT NULL
 );
