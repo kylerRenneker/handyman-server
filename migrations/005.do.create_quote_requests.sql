d447bc650352120379d39a799b0f018673d0a596
Kyler <Krenneker16@gmail.com>
2019-07-04 17:53:45 -0400

    attempting redeploy

--- migrations/005.do.create_quote_requests.sql
+++ migrations/005.do.create_quote_requests.sql
@@ -4,8 +4,7 @@ CREATE TABLE quote_requests
     provider_id INTEGER REFERENCES providers(id) ON DELETE SET NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
     location TEXT NOT NULL,
-    services INTEGER
-    [],
+    services INTEGER,
     email TEXT NOT NULL,
     description TEXT NOT NULL,
     date_created TIMESTAMP DEFAULT now
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost:5432/handyman',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/handyman',
   JWT_SECRET: 'My super secret key'
 }
